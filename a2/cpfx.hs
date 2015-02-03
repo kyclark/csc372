dc968b64a0569cd16500c7ccded2623e6dd55aa4
Ken Youens-Clark <kyclark@gmail.com>
2015-02-03 08:06:12 -0700

    working

--- a2/cpfx.hs
+++ a2/cpfx.hs
@@ -1,7 +1,9 @@
 cpfx :: [[Char]] -> [Char]
 cpfx [] = ""
 cpfx [x] = x
---cpfx (x:xs) = 
+cpfx all@(x:xs) = 
+    t bits all
+    where bits = splits x
 
 heads :: Int -> [[Char]] -> [[Char]]
 heads 0 _ = []
@@ -13,3 +15,14 @@ eqs [] = True
 eqs (x:xs) 
     | null xs = True
     | otherwise = x == head xs && eqs xs
+
+splits :: [a] -> [([a], [a])]
+splits [_] = error "shortList"
+splits xs = f [] xs
+
+f :: [a] -> [a] -> [([a], [a])]
+f xs [] = []
+f xs (y:ys) 
+    | null ys = []
+    | otherwise = (zs, ys) : f zs ys
+        where zs = xs ++ [y]
