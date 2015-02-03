34c2e8583a0fbb27770381b8dbed02f2825a5225
Ken Youens-Clark <kyclark@gmail.com>
2015-02-02 19:00:48 -0700

    progress, but no solution

--- a2/cpfx.hs
+++ a2/cpfx.hs
@@ -1,13 +1,15 @@
 cpfx :: [[Char]] -> [Char]
 cpfx [] = ""
 cpfx [x] = x
+--cpfx (x:xs) = 
 
-heads :: [[Char]] -> [Char]
-heads [] = []
-heads (x:xs) = head x : heads xs 
+heads :: Int -> [[Char]] -> [[Char]]
+heads 0 _ = []
+heads _ [] = []
+heads n (x:xs) = [(take n x)] ++ heads n xs
 
-eqs :: [Char] -> Bool
-eqs "" = True
+eqs :: [[Char]] -> Bool
+eqs [] = True
 eqs (x:xs) 
     | null xs = True
     | otherwise = x == head xs && eqs xs
