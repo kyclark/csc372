b06b757110d8a0ecc7b68884a8518dba64962cb8
Ken Youens-Clark <kyclark@gmail.com>
2015-02-08 18:58:51 -0700

    got cpfx working

--- a2/cpfx.hs
+++ a2/cpfx.hs
@@ -1,30 +1,13 @@
-{-
 cpfx :: [[Char]] -> [Char]
 cpfx [] = ""
 cpfx [x] = x
-cpfx all@(x:xs) = 
-    t bits all
-    where bits = splits x
--}
+cpfx (x:y:zs) 
+    | null common = cpfx []
+    | otherwise = cpfx ([common] ++ zs)
+    where common = cpfx2 (zip x y) []
 
-heads :: Int -> [[Char]] -> [[Char]]
-heads 0 _ = []
-heads _ [] = []
-heads n (x:xs) = [(take n x)] ++ heads n xs
-
-eqs :: [[Char]] -> Bool
-eqs [] = True
-eqs (x:xs) 
-    | null xs = True
-    | otherwise = x == head xs && eqs xs
-
-splits :: [a] -> [([a], [a])]
-splits [_] = error "shortList"
-splits xs = f [] xs
-
-f :: [a] -> [a] -> [([a], [a])]
-f xs [] = []
-f xs (y:ys) 
-    | null ys = []
-    | otherwise = (zs, ys) : f zs ys
-        where zs = xs ++ [y]
+cpfx2 :: [(Char,Char)] -> [Char] -> [Char]
+cpfx2 [] acc = acc
+cpfx2 ((x,y):zs) acc 
+    | x == y = cpfx2 zs (acc ++ [x])
+    | otherwise = cpfx2 zs acc
